11fdf0287f641e19fa9f69c29dce2fe421e2e7bc
Jean <jjeng@fec.gov>
2018-06-05 10:39:36 -0400

    add migration file to drop ofec_sched_b_objrcts

--- data/migrations/V0083__drop_ofec_sched_b_objects.sql
+++ data/migrations/V0083__drop_ofec_sched_b_objects.sql
@@ -1,11 +1,26 @@
 
 -- ------------------
+-- 	functions and triggers
+-- ------------------
+DROP TRIGGER nml_sched_b_after_trigger ON disclosure.nml_sched_b;
+DROP TRIGGER nml_sched_b_before_trigger ON disclosure.nml_sched_b;
+
+DROP TRIGGER f_item_sched_b_before_trigger ON disclosure.f_item_receipt_or_exp;
+DROP TRIGGER f_item_sched_b_after_trigger ON disclosure.f_item_receipt_or_exp;
+
+drop function public.ofec_sched_b_delete_update_queues();
+drop function public.ofec_sched_b_insert_update_queues();
+
+drop function public.ofec_sched_b_update_queues();
+drop function public.ofec_sched_b_update_fulltext();
+drop function public.ofec_sched_b_update();
+
+-- ------------------
 -- 	tables
 -- ------------------
 drop table if exists public.ofec_sched_b_queue_new;
 drop table if exists public.ofec_sched_b_queue_old;
 drop table if exists public.ofec_sched_b_fulltext;
-drop table if exists public.ofec_sched_b_master;
 drop table if exists public.ofec_sched_b_1977_1978;
 drop table if exists public.ofec_sched_b_1979_1980;
 drop table if exists public.ofec_sched_b_1981_1982;
@@ -27,13 +42,7 @@ drop table if exists public.ofec_sched_b_2011_2012;
 drop table if exists public.ofec_sched_b_2013_2014;
 drop table if exists public.ofec_sched_b_2015_2016;
 drop table if exists public.ofec_sched_b_2017_2018;
+drop table if exists public.ofec_sched_b_master;
 
--- ------------------
--- 	functions
--- ------------------
-drop function public.ofec_sched_b_delete_update_queues;
-drop function public.ofec_sched_b_insert_update_queues;
-drop function public.ofec_sched_b_update_queues;
-drop function public.ofec_sched_b_update_fulltext;
-drop function public.ofec_sched_b_update;
+
 
